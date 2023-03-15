270cfee36bd19ef67cd971a370eb63819fbc9549
Asumji <timir0709@gmail.com>
2023-03-15 17:59:48 +0100

    quests

--- commands/channels.js
+++ commands/channels.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 const fs = require("node:fs");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("channel")
-		.setDescription("Setzt verschiedene Kanäle.")
+		.setDescription("Setzt verschiedene Kanäle. (Manage Server Permissions)")
         .addSubcommand(sub => 
             sub.setName("crewdisplay")
             .setDescription("Setze den Kanal für eine automatische Crew Liste")
@@ -14,6 +14,13 @@ module.exports = {
             sub.setName("redeemitem")
             .setDescription("Setze den Kanal für Item-Tickets")
             .addChannelOption(option => option.setName("channel").setDescription("Der Forumkanal in dem das Ticket geöffnet wird.").setRequired(true))
+        )
+        .addSubcommand(sub => 
+            sub.setName("quests")
+            .setDescription("Setze den Kanal für die täglichen Crew-Aufgaben.")
+            .addChannelOption(option => option.setName("channel").setDescription("Der Kanal in dem die Aufgaben gesendet werden.").setRequired(true))
+            .addIntegerOption(option => option.setName("time").setDescription("Die Uhrzeit wann die Aufgaben gesendet werden.").setRequired(true))
+            .addChannelOption(option => option.setName("media").setDescription("Der Kanal in dem User Bilder senden.").setRequired(true))
         ),
     async execute(interaction) {
         const { crewDB, channelDB } = require("../index.js")
@@ -32,50 +39,84 @@ module.exports = {
             }
             return string
         }
-        if (interaction.options._subcommand == "crewdisplay") {
-            if (interaction.options.getChannel("channel").type == 0) {
-                if (interaction.options.getChannel("channel").manageable) {
-                    const embed = new EmbedBuilder()
-                    .setColor("Random")
-                    .setTitle("Alle Crews")
-                    .setDescription(getCrews())
-                    interaction.options.getChannel("channel").send({embeds:[embed]}).then(msg => {
-                        msg.pin()
-                        channelDB["allcrews"] = {
-                            message: msg.id,
-                            id: interaction.options.getChannel("channel").id,
-                            interval: 10000
+        if (interaction.guild.members.cache.get(interaction.user.id).permissions.has(PermissionFlagsBits.ManageGuild)) {
+            if (interaction.options._subcommand == "crewdisplay") {
+                if (interaction.options.getChannel("channel").type == 0) {
+                    if (interaction.options.getChannel("channel").manageable) {
+                        const embed = new EmbedBuilder()
+                        .setColor("Random")
+                        .setTitle("Alle Crews")
+                        .setDescription(getCrews())
+                        interaction.options.getChannel("channel").send({embeds:[embed]}).then(msg => {
+                            msg.pin()
+                            channelDB["allcrews"] = {
+                                message: msg.id,
+                                id: interaction.options.getChannel("channel").id,
+                                interval: 10000
+                            }
+
+                            fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
+                                console.log(err);
+                            });
+
+                            interaction.reply({content:"Der Kanal <#" + interaction.options.getChannel("channel") + "> wurde als Anzeigekanal gesetzt!",ephemeral: true})
+                        })
+                    } else {
+                        interaction.reply({content:"Ich habe für diesen Kanal nicht ausreichend Rechte.",ephemeral: true})
+                    }
+                } else {
+                    interaction.reply({content:"Der Kanal muss ein Textkanal sein.",ephemeral: true})
+                }
+            } else if (interaction.options._subcommand == "redeemitem") {
+                if (interaction.options.getChannel("channel").type == 15) {
+                    if (interaction.options.getChannel("channel").manageable) {
+                        channelDB["redeemitem"] = {
+                            id: interaction.options.getChannel("channel").id
                         }
 
                         fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
                             console.log(err);
                         });
-
-                        interaction.reply({content:"Der Kanal <#" + interaction.options.getChannel("channel") + "> wurde als Anzeigekanal gesetzt!",ephemeral: true})
-                    })
-                } else {
-                    interaction.reply({content:"Ich habe für diesen Kanal nicht ausreichend Rechte.",ephemeral: true})
-                }
-            } else {
-                interaction.reply({content:"Der Kanal muss ein Textkanal sein.",ephemeral: true})
-            }
-        } else if (interaction.options._subcommand == "redeemitem") {
-            if (interaction.options.getChannel("channel").type == 15) {
-                if (interaction.options.getChannel("channel").manageable) {
-                    channelDB["redeemitem"] = {
-                        id: interaction.options.getChannel("channel").id
+                        interaction.options.getChannel("channel").threads.create({name:"Item-Einlösung", message:{content:"In diesem Forumkanal werden alle Tickets geöffnet wenn Spieler Items kaufen."}})
+                    } else {
+                        interaction.reply({content:"Ich habe für diesen Kanal nicht ausreichend Rechte.",ephemeral: true})
                     }
-
-                    fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
-                        console.log(err);
-                    });
-                    interaction.options.getChannel("channel").threads.create({name:"Item-Einlösung", message:{content:"In diesem Forumkanal werden alle Tickets geöffnet wenn Spieler Items kaufen."}})
                 } else {
-                    interaction.reply({content:"Ich habe für diesen Kanal nicht ausreichend Rechte.",ephemeral: true})
+                    interaction.reply({content:"Der Kanal muss ein Forumkanal sein.",ephemeral: true})
                 }
-            } else {
-                interaction.reply({content:"Der Kanal muss ein Forumkanal sein.",ephemeral: true})
+            } else if (interaction.options._subcommand == "quests") {
+                if (interaction.options.getChannel("channel").type == 0) {
+                    if (interaction.options.getChannel("channel").manageable) {
+                        if (interaction.options.getInteger("time") > 0 && interaction.options.getInteger("time") < 25) {
+                            channelDB["quests"] = {
+                                lastSunday: null,
+                                guildid: interaction.guild.id,
+                                id: interaction.options.getChannel("channel").id,
+                                time: interaction.options.getInteger("time"),
+                                mediaid: interaction.options.getChannel("media").id,
+                                sent: false,
+                                msgs: 0,
+                                inv: 0,
+                                voice: 0,
+                                pics: 0
+                            }
+
+                            fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
+                                console.log(err);
+                            });
+                            interaction.reply({content:"Der Kanal <#" + interaction.options.getChannel("channel") + "> wurde als Aufgabenkanal gesetzt und User müssen Bilder in <#" + interaction.options.getChannel("media") + "> senden.",ephemeral:true})
+                        } else {
+                            interaction.reply({content:"Die Uhrzeit muss zwischen 1 und 24 liegen.",ephemeral: true})
+                        }
+                    } else {
+                        interaction.reply({content:"Ich habe für diesen Kanal nicht ausreichend Rechte.",ephemeral: true})
+                    }
+                } else {
+                    interaction.reply({content:"Der Kanal muss ein Textkanal sein.",ephemeral: true})
+                }
             }
+        } else {
+            interaction.reply({content:"Du hast die Rechte zu diesen Command nicht!", ephemeral:true})
         }
     }
 }
--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const path = require('node:path');
 const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages] });
+const boltEmoji = "<:blitz:1084446091698778154>"
+const arrowEmoji = "<:arrow:1084446088938917889>"
+const goldArrowEmoji = "<:arrow2:1084873571416932372>"
+const goldBoltEmoji = "<:blitz2:1084873573736398888>"
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildVoiceStates] });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
@@ -23,12 +28,265 @@ function db(file) {
 const crewDB = db("crew.json")
 const shopDB = db("shop.json")
 const channelDB = db("channels.json")
+const questDB = db("quests.json")
 module.exports = { crewDB, shopDB, client, channelDB }
+
+function getCrew(userID) {
+	for (let crew in crewDB) {
+		if (crewDB[crew].owner == userID || crewDB[crew].members.includes(userID)) {
+			return crew
+		}
+	}
+	return undefined
+}
+
+let is2ndSundayLet = false
+function is2ndSunday() {
+	const date = new Date(Date.now())
+	if (date.getDay() == 0) {
+		if (channelDB["quests"]) {
+			if (channelDB["quests"].lastSunday == null) {
+				channelDB["quests"].lastSunday = Math.floor(date / 86400000 - 7)
+
+				fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
+					console.log(err);
+				});
+
+				return false
+			} else {
+				if ((Math.floor(date / 86400000) - channelDB["quests"].lastSunday) == 14) {
+					channelDB["quests"].lastSunday = Math.floor(date / 86400000)
+
+					fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
+						console.log(err);
+					});
+
+					return true 
+				}
+			}
+		}
+	}
+}
+
+function getRndInt(min,max) { return Math.floor(Math.random() * (max - min)) + min }
+
+function generateQuests() {
+	let msgs, invites, voice, pics
+	if (is2ndSundayLet) {
+		msgs = getRndInt(50,101)
+		invites = getRndInt(1,4)
+		voice = getRndInt(2,5)
+		pics = getRndInt(1,6)
+	} else {
+		msgs = getRndInt(100,301)
+		invites = getRndInt(2,6)
+		voice = getRndInt(4,7)
+		pics = getRndInt(5,11)
+	}
+
+	channelDB["quests"] = {
+		lastSunday: channelDB["quests"].lastSunday,
+		guildid: channelDB["quests"].guildid,
+        id: channelDB["quests"].id,
+        time: channelDB["quests"].time,
+		mediaid: channelDB["quests"].mediaid,
+		sent: channelDB["quests"].sent,
+		msgs: msgs,
+		inv: invites,
+		voice: voice,
+		pics: pics
+	}
+
+	fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
+		console.log(err);
+	});
+
+	if (is2ndSundayLet) {
+		is2ndSundayLet = false
+		//return `${goldArrowEmoji} **Nachrichten:** ${msgs.toString()} | **Wert:** 30 Punkte\n\n${goldArrowEmoji} **Einladungen:** ${invites.toString()} | **Wert:** 30 Punkte\n\n${goldArrowEmoji} **Voice-Stunden:** ${voice.toString()} | **Wert:** 30 Punkte\n\n${goldArrowEmoji} **Bilder in Medien:** ${pics.toString()} | **Wert:** 30 Punkte`
+		return `${goldArrowEmoji} **Nachrichten:** ${msgs.toString()} | **Wert:** 30 Punkte\n\n${goldArrowEmoji} **Voice-Stunden:** ${voice.toString()} | **Wert:** 30 Punkte\n\n${goldArrowEmoji} **Bilder in Medien:** ${pics.toString()} | **Wert:** 30 Punkte`
+	} else {
+		//return `${arrowEmoji} **Nachrichten:** ${msgs.toString()} | **Wert:** 15 Punkte\n\n${arrowEmoji} **Einladungen:** ${invites.toString()} | **Wert:** 15 Punkte\n\n${arrowEmoji} **Voice-Stunden:** ${voice.toString()} | **Wert:** 15 Punkte\n\n${arrowEmoji} **Bilder in Medien:** ${pics.toString()} | **Wert:** 15 Punkte`
+		return `${arrowEmoji} **Nachrichten:** ${msgs.toString()} | **Wert:** 15 Punkte\n\n${arrowEmoji} **Voice-Stunden:** ${voice.toString()} | **Wert:** 15 Punkte\n\n${arrowEmoji} **Bilder in Medien:** ${pics.toString()} | **Wert:** 15 Punkte`
+	}
+}
+
+async function amountOfUsersInChannel(id) {
+	let amount = 0
+	if (channelDB["quests"]) {
+		const guild = await client.guilds.fetch(channelDB["quests"].guildid)
+		const voiceMembers = guild.members.cache.filter(member => member.voice.channel)
+		voiceMembers.forEach(async member => {
+			if (member.voice.channelId == id) {
+				amount++
+			}
+		})
+	}
+	return amount
+}
+
+function floatingPointAddition(num) {
+	if (!num.toString().includes(".")) {
+		num = Number(num.toString() + ".1")
+	} else {
+		front = num.toString().split(".")[0]
+		num = Number(num.toString().split(".")[1])
+		if (num == 9) {
+			front++
+			num = Number(front.toString() + ".0")
+		} else {
+			num++
+			num = Number(front + "." + num.toString())
+		}
+	}
+	return num
+}
+
+setInterval(async () => {
+	if (channelDB["quests"]) {
+		const guild = await client.guilds.fetch(channelDB["quests"].guildid)
+		const voiceMembers = guild.members.cache.filter(member => member.voice.channel)
+		voiceMembers.forEach(async member => {
+			if (member.voice.selfMute == false && member.voice.serverMute == false && getCrew(member.id) != undefined) {
+				if (await amountOfUsersInChannel(member.voice.channelId) >= 2) {
+					if (!questDB[member.id]) {
+						questDB[member.id] = {
+							msgs: 0,
+							inv: 0,
+							voice: 0.1,
+							pics: 0
+						}
+					} else {
+						questDB[member.id].voice = floatingPointAddition(questDB[member.id].voice)
+					}
+
+					fs.writeFileSync("./databases/quests.json", JSON.stringify(questDB, null, 4), err => {
+						console.log(err);
+					});
+				}
+			}
+		})
+	}
+}, 360000);
 
 client.once('ready', () => {
 	console.log('Ready!');
+
+	setInterval(() => {
+		if (channelDB["quests"] && client.guilds.cache.get(channelDB["quests"].guildid).channels.cache.get(channelDB["quests"].id) != undefined) {
+			const time = new Date(Date.now()).toLocaleTimeString("de-DE")
+			if (time.toString().split(":")[0] == channelDB["quests"].time.toString() && channelDB["quests"].sent == false) {
+				if (!is2ndSunday()) {
+					for (user in questDB) {
+						if (questDB[user].msgs >= channelDB["quests"].msgs && channelDB["quests"].msgs != 0) {
+							crewDB[getCrew(user)].tokens += 15
+
+							fs.writeFileSync("./databases/crew.json", JSON.stringify(crewDB, null, 4), err => {
+								console.log(err);
+							});
+						}
+						if (questDB[user].voice >= channelDB["quests"].voice && channelDB["quests"].voice != 0) {
+							crewDB[getCrew(user)].tokens += 15
+
+							fs.writeFileSync("./databases/crew.json", JSON.stringify(crewDB, null, 4), err => {
+								console.log(err);
+							});
+						}
+						if (questDB[user].pics >= channelDB["quests"].pics && channelDB["quests"].pics != 0) {
+							crewDB[getCrew(user)].tokens += 15
+
+							fs.writeFileSync("./databases/crew.json", JSON.stringify(crewDB, null, 4), err => {
+								console.log(err);
+							});
+						}
+
+						questDB[user] = {
+							msgs: 0,
+							inv: 0,
+							voice: 0,
+							pics: 0
+						}
+	
+						fs.writeFileSync("./databases/quests.json", JSON.stringify(questDB, null, 4), err => {
+							console.log(err);
+						});
+					}
+				} else {
+					is2ndSundayLet = true
+					for (user in questDB) {
+						if (questDB[user].msgs >= channelDB["quests"].msgs && channelDB["quests"].msgs != 0) {
+							crewDB[getCrew(user)].tokens += 30
+
+							fs.writeFileSync("./databases/crew.json", JSON.stringify(crewDB, null, 4), err => {
+								console.log(err);
+							});
+						}
+						if (questDB[user].voice >= channelDB["quests"].voice && channelDB["quests"].voice != 0) {
+							crewDB[getCrew(user)].tokens += 30
+
+							fs.writeFileSync("./databases/crew.json", JSON.stringify(crewDB, null, 4), err => {
+								console.log(err);
+							});
+						}
+						if (questDB[user].pics >= channelDB["quests"].pics && channelDB["quests"].pics != 0) {
+							crewDB[getCrew(user)].tokens += 30
+
+							fs.writeFileSync("./databases/crew.json", JSON.stringify(crewDB, null, 4), err => {
+								console.log(err);
+							});
+						}
+
+						questDB[user] = {
+							msgs: 0,
+							inv: 0,
+							voice: 0,
+							pics: 0
+						}
+	
+						fs.writeFileSync("./databases/quests.json", JSON.stringify(questDB, null, 4), err => {
+							console.log(err);
+						});
+					}
+				}
+
+				if (is2ndSundayLet) {
+					const embed = new EmbedBuilder()
+					.setTitle(goldBoltEmoji + " Goldene-Aufgaben " + goldBoltEmoji)
+					.setColor(0xd6a70f)
+					.setDescription(generateQuests())
+					.setFooter({text:"Die Aufgaben werden jeden Tag um " + channelDB["quests"].time.toString() + " Uhr resettet! Viel Glück! (Jeden 2. Sonntag gibt es Goldene Aufgaben)",iconURL:"https://i.imgur.com/o1CnNtX.png"})
+
+					client.guilds.cache.get(channelDB["quests"].guildid).channels.cache.get(channelDB["quests"].id).send({embeds:[embed]})
+				} else {
+					const embed = new EmbedBuilder()
+					.setTitle(boltEmoji + " Aufgaben " + boltEmoji)
+					.setColor(0xef3d63)
+					.setDescription(generateQuests())
+					.setFooter({text:"Die Aufgaben werden jeden Tag um " + channelDB["quests"].time.toString() + " Uhr resettet! Viel Glück!",iconURL:"https://i.imgur.com/o1CnNtX.png"})
+
+					client.guilds.cache.get(channelDB["quests"].guildid).channels.cache.get(channelDB["quests"].id).send({embeds:[embed]})
+				}
+
+				channelDB["quests"].sent = true
+
+				fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
+					console.log(err);
+				});
+			} else {
+				if (time.toString().split(":")[0] != channelDB["quests"].time.toString()) {
+					channelDB["quests"].sent = false
+
+					fs.writeFileSync("./databases/channels.json", JSON.stringify(channelDB, null, 4), err => {
+						console.log(err);
+					});
+				}
+			}
+		}
+	}, 10000);
 });
 
+let onMsgCD = {}
+let onPicCD = {}
 client.on("messageCreate", async message => {
 	if (message.guild != null && message.author.id != client.user.id) {
 		if (channelDB["redeemitem"] && message.guild.channels.cache.get(channelDB["redeemitem"].id) != undefined) {
@@ -67,6 +325,77 @@ client.on("messageCreate", async message => {
 						.setColor(0x9CDFBD)
 						.setDescription(message.content)
 						client.guilds.cache.get(channelDB[channel].guildid).channels.cache.get(channelDB[channel].threadID).send({embeds:[embed]})
+					}
+				}
+			}
+		}
+	}
+
+	if (message.author.id != client.user.id && message.guild != null) {
+		if(!onMsgCD[message.author.id]) {
+			onMsgCD[message.author.id] = {
+				cd: false,
+				timeouted: false
+			}
+		}
+		if (getCrew(message.author.id) != undefined) {
+			if (onMsgCD[message.author.id].cd == true && onMsgCD[message.author.id].timeouted == false) {
+				onMsgCD[message.author.id].timeouted = true
+				setTimeout(() => {
+					onMsgCD[message.author.id].cd = false
+					onMsgCD[message.author.id].timeouted = false
+				}, 5000);
+			}
+			if (channelDB["quests"] && message.guild.id == channelDB["quests"].guildid && channelDB["quests"].msgs != 0 && onMsgCD[message.author.id].cd == false) {
+				if (!questDB[message.author.id]) {
+					questDB[message.author.id] = {
+						msgs: 1,
+						inv: 0,
+						voice: 0,
+						pics: 0
+					}
+				} else {
+					questDB[message.author.id].msgs += 1
+				}
+				fs.writeFileSync("./databases/quests.json", JSON.stringify(questDB, null, 4), err => {
+					console.log(err);
+				});
+				onMsgCD[message.author.id].cd = true
+			}
+		}
+	}
+	if (message.author.id != client.user.id && message.guild != null) {
+		if (message.channel.id == channelDB["quests"].mediaid) {
+			if (message.attachments.size != 0) {
+				if(!onPicCD[message.author.id]) {
+					onPicCD[message.author.id] = {
+						cd: false,
+						timeouted: false
+					}
+				}
+				if (getCrew(message.author.id) != undefined) {
+					if (onPicCD[message.author.id].cd == true && onPicCD[message.author.id].timeouted == false) {
+						onPicCD[message.author.id].timeouted = true
+						setTimeout(() => {
+							onPicCD[message.author.id].cd = false
+							onPicCD[message.author.id].timeouted = false
+						}, 5000);
+					}
+					if (channelDB["quests"] && message.guild.id == channelDB["quests"].guildid && channelDB["quests"].pics != 0 && onPicCD[message.author.id].cd == false) {
+						if (!questDB[message.author.id]) {
+							questDB[message.author.id] = {
+								msgs: 0,
+								inv: 0,
+								voice: 0,
+								pics: 1
+							}
+						} else {
+							questDB[message.author.id].pics += 1
+						}
+						fs.writeFileSync("./databases/quests.json", JSON.stringify(questDB, null, 4), err => {
+							console.log(err);
+						});
+						onPicCD[message.author.id].cd = true
 					}
 				}
 			}
